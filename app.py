fe9c92b5379a485f13639670dac8a440d68267f7
ApurvJDev <jadhavapurv34@gmail.com>
2024-08-10 18:15:28 +0530

    Wrote function to extract text from pdf

--- app.py
+++ app.py
@@ -18,4 +18,9 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_pdf_text(pdf_docs):
     text=""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
     
